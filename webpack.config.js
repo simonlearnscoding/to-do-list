1f9e786a3c01a59b2f4936172bd4c341544eef9a
simonlearnscoding <simon.muscas@gmail.com>
2023-07-17 13:41:40 +0200

    fixed npm run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-
-
-
-    devtool: 'inline-source-map',
-
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-        }),
+  mode: "development",
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
+  },
 
+  devtool: "inline-source-map",
 
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/',
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    devtool: 'inline-source-map',
-}
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "./src/index.html",
+    }),
+  ],
+
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "",
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  devtool: "inline-source-map",
+};
